dc6ad1435130218916c28742d770ef2ff112c42c
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-01-21 20:42:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conexao = mysql.connector.connect(
+conexao = mysql.connector.connect(      #conexão com bancos de dados
   # host='localhost',
   # user='root',
   # password='123456',
@@ -23,30 +23,30 @@ conexao.commit() # edita o banco de dados
 cursor.close()
 conexao.close()
 
-# CREATE
+# CREATE          
 
-nome_produto = "chocolate"
+nome_produto = "chocolate"       # cria itens dentro do banco
 valor = 15
 comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
 cursor.execute(comando)
 conexao.commit() # edita o banco de dados
 
 
-# READ
+# READ                             # Lê o dados no banco
 comando = f'SELECT * FROM vendas'
 cursor.execute(comando)
 resultado = cursor.fetchall() # ler o banco de dados
 print(resultado)
 
-
-# UPDATE
+ 
+# UPDATE                     # Atualiza o banco
 nome_produto = "todynho"
 valor = 6
 comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
 cursor.execute(comando)
 conexao.commit() # edita o banco de dados
 
-# DELETE
+# DELETE                       # Exclui itens
 nome_produto = "todynho"
 comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
 cursor.execute(comando)
